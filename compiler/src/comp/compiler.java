2eb0b1942d822c3a253836dd6d73631369bf8904
kiana-kbta <kiana112347@gmail.com>
2022-11-25 19:16:00 +0330

    comp directory and input named "code.trl" are added

---
+++ compiler/src/comp/compiler.java
@@ -0,0 +1,33 @@
+package comp;
+
+import gen.ToorlaLexer;
+import gen.ToorlaListener;
+import gen.ToorlaParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+
+import java.io.IOException;
+
+public class compiler {
+    public static void main(String[]args) throws IOException{
+
+        CharStream stream= CharStreams.fromFileName("code.trl");
+        ToorlaLexer lexer=new ToorlaLexer(stream);
+        TokenStream tokens=new CommonTokenStream(lexer);
+        ToorlaParser parser=new ToorlaParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree=parser.program();
+        ParseTreeWalker walker=new ParseTreeWalker();
+        ToorlaListener listener=new ProgramPrinter();
+//        walker.walk(listener,tree);
+
+
+
+
+
+
+
+    }
+}
